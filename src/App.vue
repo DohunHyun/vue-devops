5a1a460d3f3c7b4617b78e8c4b5d985c287a43f4
DohunHyun <dohun.hyun@gmail.com>
2021-06-29 18:07:21 +0900

    Fix props msg

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to 후니현! Vue.js App" />
+  <HelloWorld msg="Welcome to 후니현! Vue.js App 테스트 성공! " />
 </template>
 
 <script>
